7e2067e14f7e79b60b730318e55b0bea188806e4
tgraha09 <tfire09@gmail.com>
2023-02-02 14:16:57 -0500

    Updated package

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = require('./router')
 const app = express()
-
+const favicon = require('express-favicon');
 
 app.use(express.json())
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 router(app)
 

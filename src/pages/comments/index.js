274770098166003f7e3126995022198f64affdbb
tgraha09 <tfire09@gmail.com>
2023-02-03 16:58:13 -0500

    Added api for next

--- src/pages/comments/index.js
+++ src/pages/comments/index.js
@@ -2,12 +2,18 @@ import { useState } from "react";
 
 function CommentsPage(){
 
-    const [comments, setComments] = useState([])
+    const [comments, setComments, quotes, SetQuotes] = useState([])
 
     const fetchComments = async () =>{
         const response = await fetch('/api/comments')
         const data = await response.json()
         setComments(data)
+    }
+    const fetchQuotes = async () =>{
+        const response = await fetch('https://poetrydb.org/title/Ozymandias/lines.json')
+        const data = await response.json()
+        console.log(data);
+        //SetQuotes(data)
     }
     return (
         <>
@@ -21,8 +27,42 @@ function CommentsPage(){
                     )
                 })
             }
+            <Poetry></Poetry>
         </>
     )
 }
+
+function Poetry(){
+
+    const [quotes, SetQuotes] = useState([])
+
+    const fetchQuotes = async () =>{
+        const response = await fetch('https://poetrydb.org/title/Ozymandias/lines.json')
+        const data = await response.json()
+        
+       /* console.log(data[0].lines);
+        data[0].lines.map(quote => {
+            console.log(quote);
+        })*/
+        SetQuotes(data[0].lines)
+    }
+    return (
+        <>
+            <button onClick={fetchQuotes}>Load Quotes</button>
+            {
+                
+                quotes.map(quote => {
+                    return (
+                        <div>
+                            {quote}
+                        </div>
+                    )
+                })
+            }
+        </>
+    )
+}
+
+
 
 export default CommentsPage

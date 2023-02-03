e6347576021d207cd4cdb6178975b854f400e9b6
tgraha09 <tfire09@gmail.com>
2023-02-03 16:15:13 -0500

    Added api for next

---
+++ src/pages/login/login.js
@@ -0,0 +1,13 @@
+export default function Login() {
+    return (
+      <>
+        <Head>
+          
+          <meta name="description" content="Next-Express-App" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <h1>Login</h1>
+      </>
+    )
+  }
---
+++ src/pages/signup/signup.js
@@ -0,0 +1,13 @@
+export default function Signup() {
+    return (
+      <>
+        <Head>
+          
+          <meta name="description" content="Next-Express-App" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <h1>Signup</h1>
+      </>
+    )
+  }
